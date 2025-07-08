84b92c8115d457f20b835a746dbba5170fce5a2f
Cezanne <cezan.git@gmail.com>
2025-07-08 23:13:16 +0530

    Time: 4 ms (91.41%), Space: 59 MB (99.99%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    let orginal = x;
+    let reverse = 0;
+    while (x > 0) {
+        reverse = reverse * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    if (orginal === reverse) {
+        return true;
+    } else {
+        return false;
+    }
+};
