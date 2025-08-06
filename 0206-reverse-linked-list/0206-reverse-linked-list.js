dc0807664f70f3ad8fe56a0847c7558dfb541a18
Cezanne <cezan.git@gmail.com>
2025-08-06 12:27:05 +0530

    Time: 0 ms (100%), Space: 56 MB (89.88%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,13 +10,13 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    let current = head;
-    let prev = null;
-    while(current){
-        let next = current.next;
-        current.next = prev;
-        prev = current;
-        current = next;
-    }
-    return prev;
+  let prev = null;
+  let current = head;
+  while(current){
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next
+  }
+  return prev;
 };
