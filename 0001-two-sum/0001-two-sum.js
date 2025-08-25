e6ef73d954564fafc622c508f7f12d4858729bcb
Cezanne <cezan.git@gmail.com>
2025-08-25 14:59:34 +0530

    Time: 4 ms (55.83%), Space: 60.3 MB (5.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0; i<nums.length-1; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i]+nums[j]===target){
-                return [i,j]
-            }
+    let map = new Map();
+    for(let [ind,ele] of nums.entries()){
+        if(map.has(target-ele)){
+            return [map.get(target-ele),ind];
+        }else{
+            map.set(ele,ind);
         }
     }
+
 };
