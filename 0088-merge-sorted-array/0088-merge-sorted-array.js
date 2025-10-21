ab65393b141142af609ce121a087ef7deee6c029
Cezanne <cezan.git@gmail.com>
2025-10-21 19:27:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,17 +5,6 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) 
-{
-
-    for(let j=0; j<n; j++)
-    {
-        nums1.pop();
-    }
-    for(let k=0; k<n; k++)
-    {
-        nums1.push(nums2[k]);
-    }
-    nums1.sort((a,b)=>a-b)
-
+var merge = function(nums1, m, nums2, n) {
+    
 };
