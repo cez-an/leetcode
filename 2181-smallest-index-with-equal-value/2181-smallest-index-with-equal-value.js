9335e2e7eeed093ef2823dd5b73843d396d30933
Cezanne <cezan.git@gmail.com>
2025-10-22 23:38:36 +0530

    Time: 0 ms (100%), Space: 50.7 MB (100%) - LeetHub

---
+++ 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var smallestEqual = function(nums) {
+   
+   let result=-1;
+
+   for(let i=0; i<nums.length; i++){
+    if(i%10==nums[i]){
+        result=i;
+        break;
+    }else{
+        result=-1;
+    }
+
+   }
+    return result;
+
+};
