cb0353d20235f153fa18e6c1e0517d3a1a559146
Cezanne <cezan.git@gmail.com>
2025-10-25 09:50:00 +0530

    Time: 0 ms (100%), Space: 54.5 MB (37.82%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.js
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.js
@@ -3,12 +3,10 @@
  * @param {character} ch
  * @return {string}
  */
- 
 var reversePrefix = function(word, ch) {
 
     if(!word.includes(ch)) return word;
-    let reversed = word.slice(0,(word.indexOf(ch)+1)).split('').reverse().join('') + word.slice((word.indexOf(ch)+1));
-    return(reversed)
-
+    return ( word.slice(0,(word.indexOf(ch)+1)).split('').reverse().join('') + word.slice((word.indexOf(ch)+1)) )
+    
 };
 
