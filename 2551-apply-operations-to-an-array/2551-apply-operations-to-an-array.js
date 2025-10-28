1480a259dee02c2556bea8bfb6005cb7f68dc999
Cezanne <cezan.git@gmail.com>
2025-10-28 09:15:09 +0530

    Time: 2 ms (58%), Space: 58 MB (41%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var applyOperations = function(nums) {
+    let result = [];
+    for(let i=0; i<nums.length-1; i++){
+        if(nums[i]===nums[i+1]){
+            nums[i]=nums[i]*2;
+            nums[i+1]=0;
+        }
+    }
+    for(let j=nums.length-1; j>=0; j--){
+        if(nums[j]!==0){
+            result.unshift(nums[j])
+        }else{
+            result.push(nums[j])
+        }
+    }
+    return result;
+};
