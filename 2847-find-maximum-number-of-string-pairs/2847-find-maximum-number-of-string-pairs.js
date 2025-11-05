7c44b22c6d160405fce35e94fb9c9496c583742a
Cezanne <cezan.git@gmail.com>
2025-11-05 23:31:05 +0530

    Time: 19 ms (5.1%), Space: 55.3 MB (95.92%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.js
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var maximumNumberOfStringPairs = function(words) {
-    let length=words.length;
     let numberOfPairs=0;
+    let length=words.length;
     for(let i=0; i<length; i++){
         for(let j=i+1;j<length; j++){
             
-                if(words[i]===(words[j].split('').reverse().join('')))
-                {
+                if(words[i]===(words[j].split('').reverse().join(''))){
                     numberOfPairs++;
                 }
-        }
+    }
     }
     return numberOfPairs;
 };
