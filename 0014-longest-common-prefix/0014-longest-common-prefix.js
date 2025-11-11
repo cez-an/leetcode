943e5e79832df195053ddbdb2a98d4585ef140aa
Cezanne <cezan.git@gmail.com>
2025-11-11 11:24:05 +0530

    Time: 6 ms (12.94%), Space: 55.4 MB (38.22%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,19 +3,23 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let pref = strs[0];
-    let preflen = pref.length;
-    for(let i =1; i<strs.length; i++){
-    let res = strs[i];
 
-    while(pref!==res.substring(0,preflen))
-    {
-        preflen--;
-        if(preflen===0){
-            return "";
+    let res = strs[0];
+    let resLength = res.length;
+    for(let i=1; i<strs.length; i++){
+
+        let com = strs[i];
+
+        while(res!==com.substring(0,resLength)){
+            resLength--;
+            if(resLength === 0){
+                return "";
+            }
+            res = res.substring(0,resLength);
+            console.log(res)
         }
-        pref = pref.substring(0,preflen);
+
+
     }
-}
-return pref;
+    return res;
 };
