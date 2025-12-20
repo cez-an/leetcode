9942eec5b909cfaa6dce977401ab13d71b6557a0
Cezanne <cezan.git@gmail.com>
2025-12-20 21:56:57 +0530

    Time: 2 ms (72.28%), Space: 59.4 MB (5.45%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.js
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.js
@@ -4,22 +4,31 @@
  */
 var minimumAverage = function(nums) {
 
+    // let avg = [];
+    // let i=0;
+    // let len=nums.length;
+
+    //     while(i<(len)/2){
+    //     let max = Math.max(...nums);
+    //     let min = Math.min(...nums);
+
+    //     avg.push((max+min)/2);
+
+    //     let larIndex = nums.indexOf(max);
+    //     nums.splice(larIndex,1);
+    //     let smlIndex = nums.indexOf(min);
+    //     nums.splice(smlIndex,1);
+    //     i++;
+    //     }
+
+    //     return Math.min(...avg)
+
     let avg = [];
-    let i=0;
-    let len=nums.length;
-
-        while(i<(len)/2){
-        let max = Math.max(...nums);
-        let min = Math.min(...nums);
-
-        avg.push((max+min)/2);
-
-        let larIndex = nums.indexOf(max);
-        nums.splice(larIndex,1);
-        let smlIndex = nums.indexOf(min);
-        nums.splice(smlIndex,1);
-        i++;
-        }
-
-        return Math.min(...avg)
+    while(nums.length>0){
+    nums.sort((a,b)=>a-b);    
+    let a = nums.shift();
+    let b = nums.pop();
+    avg.push((a+b)/2);
+    }
+    return Math.min(...avg)
 };
