2132619a588343e1466bc990e7913c0d449e4326
Cezanne <cezan.git@gmail.com>
2025-12-29 21:43:16 +0530

    Time: 0 ms (100%), Space: 54.8 MB (92.43%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    let count = 0;
+    for(let i=0; i<nums.length-1; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i]+nums[j]<target){
+                count++;
+            }
+        }
+    }
+    return count;
+};
+
+// n[i]+n[j] < target;
+
+// n>j>i>=0
+
+// n - length of the array 
