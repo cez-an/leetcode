0935bde2b0d3a465d593d65b606ec053592363d7
Cezanne <cezan.git@gmail.com>
2026-02-10 10:51:41 +0530

    Time: 6 ms (51.11%), Space: 58.1 MB (54.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,21 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    
+
     if (s.length <= 1) return true;
-
     const srt = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-
     if (srt.length <= 1) return true;
-
     let left = 0;
     let right = srt.length - 1;
-
     while (left < right) {
         if (srt[left++] !== srt[right--]) return false
     }
-
     return true;
+    
 };
 
    // console.log(srt)
