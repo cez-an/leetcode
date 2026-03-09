a6000d7bd7a674e37353c30e8fd6d8bedc10d8a2
Cezanne <cezan.git@gmail.com>
2026-03-09 09:41:40 +0530

    Time: 17 ms (66.69%), Space: 74.5 MB (40.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,7 +2,16 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var containsDuplicate = function (nums) {
+//     let set = new Set([...nums]);
+//     return set.size !== nums.length;
+// };
+
 var containsDuplicate = function (nums) {
-    let set = new Set([...nums]);
-    return set.size !== nums.length;
+    let set = new Set();
+    for(let i of nums){
+        if(set.has(i)) return true;
+        else set.add(i);
+    }
+    return false
 };
